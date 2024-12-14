aacbaf000ee5b44e3fcad4fe986e3ac8f8d5891a
ArjunkSanthosh <arjunk800432gmail.com>
2024-12-14 17:38:11 +0530

    kazhinju

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -12,18 +12,24 @@ function About() {
   }, []);
 
   return (
-    <section id="about" className="py-20 text-white shadow-md shadow-gray-200">
+    <section id="about" className="py-8 text-white shadow-md shadow-gray-200">
+      <div 
+        className="bg-cover bg-center h-full" 
+        style={{ backgroundImage: 'url("/arjun.jpg")' }} // Replace with your image path
+      ></div>
       <div className="container text-center">
-        <h2 className={`text-4xl font-serif text-black ${isVisible ? 'reveal-top' : ''}`} style={{ transitionDelay: '0.1s' }}>About Me</h2>
+        <div className='flex justify-center items-center'>
         <img 
           src="arjun.jpg" 
           alt="" 
-          className={`w-36 h-36 object-cover rounded-full m-auto mt-7 transform hover:scale-105 transition-all duration-500 hover:shadow-md hover:shadow-cyan-500 ${isVisible ? 'reveal-bottom' : ''}`} 
+          className={`w-72 h-72 object-cover rounded-full mt-28 border-4 border-gray-800 	  transform hover:scale-105 transition-all duration-500 hover:shadow-lg hover:border-cyan-500 hover:shadow-cyan-500 ${isVisible ? 'reveal-bottom' : ''}`} 
           style={{ transitionDelay: '0.2s' }}
         />
-        <h3 className={`text-l mt-5 text-gray-500 ${isVisible ? 'reveal-left' : ''}`} style={{ transitionDelay: '0.3s' }}> Hey I'm,</h3>
+       <div className='w-8/12'>
+       <h2 className={`text-7xl mt-5 font-serif text-black ${isVisible ? 'reveal-top' : ''}`} style={{ transitionDelay: '0.1s' }}>About Me</h2>
+        <h3 className={`text-l  mt-8 text-gray-500 ${isVisible ? 'reveal-left' : ''}`} style={{ transitionDelay: '0.3s' }}> Hey I'm,</h3>
         <h3 className={`text-4xl mt-2 text-black ${isVisible ? 'reveal-right' : ''}`} style={{ transitionDelay: '0.4s' }}> Arjun K Santhosh</h3>
-        <p className={`mt-4 text-base mb-12 w-9/12 m-auto text-justify indent-20 text-black ${isVisible ? 'reveal-left' : ''}`} style={{ transitionDelay: '0.5s' }}>
+        <p className={`mt-4 text-base  w-5/6 m-auto text-justify indent-20 text-black ${isVisible ? 'reveal-left' : ''}`} style={{ transitionDelay: '0.5s' }}>
           I'm a passionate web developer specializing in front-end technologies like React, JavaScript, and Tailwind CSS.
           As a passionate MERN stack developer, I specialize in creating dynamic, responsive, and user-centric web applications. 
           With expertise in MongoDB, Express.js, React, and Node.js, I enjoy building full-stack solutions that are both scalable and efficient. 
@@ -31,6 +37,8 @@ function About() {
           I am driven by a strong desire to solve complex problems and deliver seamless user experiences. 
           Whether it’s building APIs, managing databases, or crafting intuitive interfaces, I am committed to writing clean, maintainable code.
         </p>
+       </div>
+        </div>
 
         <div className="mt-28">
           <div className="bg-gray-800 text-white p-8 shadow-lg mx-auto shadow-md shadow-cyan-100">
